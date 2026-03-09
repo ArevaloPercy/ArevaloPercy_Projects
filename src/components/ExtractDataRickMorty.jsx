f39662b50612c3ae5595f172f44397dc1339306f
Percy Arevalo <percy.24.arevalo@gmail.com>
2026-03-09 01:02:51 -0500

    Cambios Rick Morty

--- src/components/ExtractDataRickMorty.jsx
+++ src/components/ExtractDataRickMorty.jsx
@@ -1,4 +1,6 @@
+
 const ExtractDataRickMorty = ({setList}) => {
+    
     const getData = async () =>{
         /*Declarar la URL a la que nos vamos a conetar */
         const URL = 'https://rickandmortyapi.com/api/character/?name=rick'
@@ -6,13 +8,18 @@ const ExtractDataRickMorty = ({setList}) => {
         /*Convertir a JSON*/
         const data = await response.json()
         console.log(data)
+        /*Guarda los personajes en el array */
         setList (data.results)
-
+        console.log(data.info.count)
+    }
+    const hideData = () =>{
+        setList([])
     }
     
     return (
         <div>
-            <button onClick={getData}  >Mostrar info</button>
+            <button onClick={getData}  >Mostrar Info</button>
+            <button onClick={hideData}>Ocultar Info</button>
         </div>
         
 
